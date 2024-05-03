997619d0de05dc12dc0c35252d140ad65765dfbb
hoseok jang <hs.jang@mongta.co.kr>
2024-05-03 16:30:43 +0900

    chapter2-5 (2)

--- src/main/java/com/example/sbb/repository/QuestionRepository.java
+++ src/main/java/com/example/sbb/repository/QuestionRepository.java
@@ -1,9 +1,15 @@
 package com.example.sbb.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.sbb.dto.Question;
 
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
 
+	Question findBySubject(String subject);
+	Question findBySubjectAndContent(String subject, String content);
+	List<Question> findBySubjectLike(String subject);
+	
 }
--- src/test/java/com/example/sbb/SbbApplicationTests.java
+++ src/test/java/com/example/sbb/SbbApplicationTests.java
@@ -1,9 +1,11 @@
 package com.example.sbb;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,23 +22,65 @@ class SbbApplicationTests {
 	
 	@Test
 	void testJpa() {
+		// row 1 insert
 //		Question q1 = new Question();
 //		q1.setSubject("sbb가 무엇인가요?");
 //		q1.setContent("sbb에 대해서 알고 싶습니다.");
 //		q1.setCreateDate(LocalDateTime.now());
 //		this.questionRepository.save(q1); // 첫번째 질문 저장
 //		
+		// row 2 insert
 //		Question q2 = new Question();
 //		q2.setSubject("스프링부트 모델 질문입니다.");
 //		q2.setContent("id는 자동으로 생성 되나요?");
 //		q2.setCreateDate(LocalDateTime.now());
 //		this.questionRepository.save(q2); // 두번째 질문 저장
 
-		List<Question> all = this.questionRepository.findAll();
-		assertEquals(2, all.size());
 		
-		Question q = all.get(0);
-		assertEquals("sbb가 무엇인가요?", q.getSubject());
+		// findAll
+//		List<Question> all = this.questionRepository.findAll();
+//		assertEquals(2, all.size());
+//		// assertEquals(기댓값, 실제값) -> DB에서 데이터를 올바르게 가져오는지 기댓값과 실제값이 동일한지를 조사
+//		
+//		Question q = all.get(0);
+//		assertEquals("sbb가 무엇인가요?", q.getSubject());
+
+		// findById
+//		Optional<Question> oq = this.questionRepository.findById(1);
+//		if (oq.isPresent()) {
+//			Question q = oq.get();
+//			assertEquals("sbb가 무엇인가요?", q.getSubject());
+//		}
+	
+		// findBySubject
+//		Question q = this.questionRepository.findBySubject("sbb가 무엇인가요?");
+//		assertEquals(1, q.getId());
+		
+		// findBySubjectAndContent
+//		Question q = this.questionRepository.findBySubjectAndContent("sbb가 무엇인가요?", "sbb에 대해서 알고 싶습니다.");
+//		assertEquals(1, q.getId());
+
+		// findBySubjectLike
+//		List<Question> qList = this.questionRepository.findBySubjectLike("sbb%");
+//		Question q = qList.get(0);
+//		assertEquals("sbb가 무엇인가요?", q.getSubject());
+
+		// 질문 데이터 수정
+//		Optional<Question> oq = this.questionRepository.findById(1);
+//		assertTrue(oq.isPresent());
+//		Question q = oq.get();
+//		q.setSubject("수정된 제목");
+//		this.questionRepository.save(q);
+
+		// 질문 데이터 삭제
+		assertEquals(2, this.questionRepository.count());
+		Optional<Question> oq = this.questionRepository.findById(1);
+		assertTrue(oq.isPresent());
+		Question q = oq.get();
+		this.questionRepository.delete(q);
+		assertEquals(1, this.questionRepository.count());
+		
+		
 	}
 
 }
