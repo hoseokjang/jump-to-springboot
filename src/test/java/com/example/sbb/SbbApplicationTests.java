13ea92d742b1f09c602664dab1eea8cb07d41e33
hoseok jang <hs.jang@mongta.co.kr>
2024-05-07 13:36:28 +0900

    chapter2-5-3

--- src/test/java/com/example/sbb/SbbApplicationTests.java
+++ src/test/java/com/example/sbb/SbbApplicationTests.java
@@ -11,8 +11,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.example.sbb.dto.Answer;
 import com.example.sbb.dto.Question;
+import com.example.sbb.repository.AnswerRepository;
 import com.example.sbb.repository.QuestionRepository;
+
+import jakarta.transaction.Transactional;
 
 @SpringBootTest
 class SbbApplicationTests {
@@ -20,7 +24,11 @@ class SbbApplicationTests {
 	@Autowired
 	private QuestionRepository questionRepository;
 	
+	@Autowired
+	private AnswerRepository answerRepository;
+	
 	@Test
+	@Transactional
 	void testJpa() {
 		// row 1 insert
 //		Question q1 = new Question();
@@ -73,13 +81,39 @@ class SbbApplicationTests {
 //		this.questionRepository.save(q);
 
 		// 질문 데이터 삭제
-		assertEquals(2, this.questionRepository.count());
-		Optional<Question> oq = this.questionRepository.findById(1);
+//		assertEquals(2, this.questionRepository.count());
+//		Optional<Question> oq = this.questionRepository.findById(1);
+//		assertTrue(oq.isPresent());
+//		Question q = oq.get();
+//		this.questionRepository.delete(q);
+//		assertEquals(1, this.questionRepository.count());
+		
+		// 답변 데이터 저장하기
+//		Optional<Question> oq = this.questionRepository.findById(2);
+//		assertTrue(oq.isPresent());
+//		Question q = oq.get();
+//		
+//		Answer a = new Answer();
+//		a.setContent("네 자동으로 생성됩니다.");
+//		a.setQuestion(q); // 어떤 질문의 답변인지 알기 위해서 Question 객체가 필요
+//		a.setCreateDate(LocalDateTime.now());
+//		this.answerRepository.save(a);
+
+		// 답변 조회하기
+//		Optional<Answer> oa = this.answerRepository.findById(1);
+//		assertTrue(oa.isPresent());
+//		Answer a = oa.get();
+//		assertEquals(2, a.getQuestion().getId());
+
+		// 답변 데이터를 통해 질문 데이터 찾기 vs 질문 데이터를 통해 답변 데이터 찾기
+		Optional<Question> oq = this.questionRepository.findById(2);
 		assertTrue(oq.isPresent());
 		Question q = oq.get();
-		this.questionRepository.delete(q);
-		assertEquals(1, this.questionRepository.count());
 		
+		List<Answer> answeList = q.getAnswerList();
+		
+		assertEquals(1, answeList.size());
+		assertEquals("네 자동으로 생성됩니다.", answeList.get(0).getContent());
 		
 	}
 
