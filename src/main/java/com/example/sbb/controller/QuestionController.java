5143a4c4de2e0ae0d2b72cbfc8201550777b8389
hoseok jang <hs.jang@mongta.co.kr>
2024-05-09 16:34:29 +0900

    chapter 2-11

--- src/main/java/com/example/sbb/controller/QuestionController.java
+++ src/main/java/com/example/sbb/controller/QuestionController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.sbb.dto.Question;
 //import com.example.sbb.repository.QuestionRepository;
@@ -15,19 +16,20 @@ import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Controller
+@RequestMapping("/question")
 public class QuestionController {
 	
 //	private final QuestionRepository questionRepository;
 	private final QuestionService questionService;
 	
-	@GetMapping("/question/list")
+	@GetMapping("/list")
 	public String list(Model model) {
 		List<Question> questionList = this.questionService.getList();
 		model.addAttribute("questionList", questionList);
 		return "question_list";
 	}
 	
-	@GetMapping("/question/detail/{id}")
+	@GetMapping("/detail/{id}")
 	public String detail(Model model, @PathVariable("id") Integer id) {
 		Question question = this.questionService.getQuestion(id);
 		model.addAttribute("question", question);
